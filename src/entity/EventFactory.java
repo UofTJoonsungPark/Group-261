cc18ff626074d2c7860fd1de929acf9a217b0167
jo <129003967+jolateral@users.noreply.github.com>
2023-11-13 02:53:38 -0500

    implemented the create method in EventFactory to create a new Event.

--- src/entity/EventFactory.java
+++ src/entity/EventFactory.java
@@ -1,4 +1,15 @@
 package entity;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class EventFactory {
+
+    public Event create(LocalDate startDate, LocalDate endDate,
+                        LocalTime startTime, LocalTime endTime,
+                        String title, String description, String location) {
+
+        return new Event(startDate, endDate, startTime, endTime, title, description, location);
+    }
+
 }
