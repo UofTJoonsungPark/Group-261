c6a8a6061b0a4ff748bb656db7bff63377624e83
jo <129003967+jolateral@users.noreply.github.com>
2023-11-13 03:17:10 -0500

    Implemented the EventController to include a method called createEvent, which calls on the EventInteractor to create a new Event.

--- src/interface_adapter/event/EventController.java
+++ src/interface_adapter/event/EventController.java
@@ -3,6 +3,9 @@ package interface_adapter.event;
 import use_case.event.EventInputBoundary;
 import use_case.event.EventInputData;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class EventController {
     final EventInputBoundary eventUseCaseInteractor;
 
@@ -14,5 +17,22 @@ public class EventController {
         if (back) {
             eventUseCaseInteractor.execute(new EventInputData(true));
         }
+    }
+
+    public void createEvent(String title, String location, String description,
+                            LocalDate startDate, LocalTime startTime,
+                            LocalDate endDate, LocalTime endTime) {
+        EventInputData eventInputData = new EventInputData(false);
+
+        eventInputData.setTitle(title);
+        eventInputData.setLocation(location);
+        eventInputData.setDescription(description);
+        eventInputData.setStartDate(startDate);
+        eventInputData.setEndDate(endDate);
+        eventInputData.setStartTime(startTime);
+        eventInputData.setEndTime(endTime);
+
+        eventUseCaseInteractor.execute(eventInputData);
+
     }
 }
