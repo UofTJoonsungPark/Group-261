16ad32de6cc4e42c45cbe7bb7bbbd4c1bb09facf
jo <129003967+jolateral@users.noreply.github.com>
2023-11-20 21:01:41 -0500

    I implemented FileEventUserDataAccessObject.makeCsvFile. This creates an empty csv file for a user if they don't have one.

--- src/data_access/FileEventUserDataAccessObject.java
+++ src/data_access/FileEventUserDataAccessObject.java
@@ -4,6 +4,9 @@ import entity.Event;
 import entity.EventFactory;
 import use_case.event.EventDataAccessInterface;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Map;
@@ -22,6 +25,24 @@ public class FileEventUserDataAccessObject implements EventDataAccessInterface {
         this.events = events;
         this.eventReference = eventReference;
         this.eventFactory = eventFactory;
+    }
+
+    private static void makeCsvFile(String filePath) {
+        try {
+            File file = new File(filePath);
+
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeMaps() {
+        ;
+    }
+
+    public void clearMaps() {
+        ;
     }
 
 
