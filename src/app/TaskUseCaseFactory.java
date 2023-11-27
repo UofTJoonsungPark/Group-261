9d83a6e2f2c5287b190a35dd78ac8d80b8855525
nargesmn100 <nargesmn004@gmail.com>
2023-11-26 20:57:54 -0500

    WAS UNABLE TO FIX TASKUSECASE FACTORY!!!

--- src/app/TaskUseCaseFactory.java
+++ src/app/TaskUseCaseFactory.java
@@ -3,41 +3,33 @@ package app;
 import data_access.FileTaskUserDataAccessObject;
 import entity.TaskFactory;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.task.TaskController;
 import interface_adapter.task.TaskPresenter;
 import interface_adapter.task.TaskViewModel;
 import use_case.task.TaskDataAccessInterface;
 import use_case.task.TaskInputBoundary;
 import use_case.task.TaskInteractor;
-import use_case.task.TaskOutputBoundary;
-import view.TaskView;
 
 import java.util.HashMap;
 
 public class TaskUseCaseFactory {
     private TaskUseCaseFactory() {}
 
-    public static TaskView create(
-            ViewManagerModel viewManagerModel,
-            TaskViewModel taskViewModel,
-            LoggedInViewModel loggedInViewModel
+    public static TaskViewModel create(
+            ViewManagerModel viewManagerModel
     ) {
-        TaskController taskController = createTaskUseCase(viewManagerModel, taskViewModel, loggedInViewModel);
-        return new TaskView(taskViewModel, taskController);
+        TaskController taskController = createTaskUseCase(viewManagerModel);
+        return new TaskViewModel();
     }
 
     private static TaskController createTaskUseCase(
-            ViewManagerModel viewManagerModel,
-            TaskViewModel taskViewModel,
-            LoggedInViewModel loggedInViewModel
+            ViewManagerModel viewManagerModel
     ) {
-        TaskOutputBoundary taskOutputBoundary = new TaskPresenter(
-                taskViewModel, viewManagerModel);
+        TaskPresenter taskPresenter = new TaskPresenter(TaskViewModel);
         TaskFactory taskFactory = new TaskFactory();
-        TaskDataAccessInterface taskDataAccessObject = new FileTaskUserDataAccessObject(new HashMap<>(), taskFactory);
+        TaskInputBoundary taskInteractor = new TaskInteractor(taskPresenter, taskFactory, taskDataAccessObject);
 
-        TaskInputBoundary taskInputBoundary = new TaskInteractor(taskOutputBoundary, taskFactory, taskDataAccessObject);
-        return new TaskController(taskInputBoundary, (TaskPresenter) taskOutputBoundary);
+        TaskInputBoundary taskInteractor = new TaskInteractor();
+        return new TaskController(taskInteractor, TaskInputBoundary);
     }
 }
