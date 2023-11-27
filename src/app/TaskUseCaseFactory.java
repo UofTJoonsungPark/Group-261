c850428a480787da808c1c0ef75a5186c0557851
nargesmn100 <nargesmn004@gmail.com>
2023-11-26 20:12:21 -0500

    implemented task use case factory, will not run until taskpresenter has been implemented

--- src/app/TaskUseCaseFactory.java
+++ src/app/TaskUseCaseFactory.java
@@ -1,2 +1,43 @@
-package app;public class TaskUseCaseFactory {
+package app;
+
+import data_access.FileTaskUserDataAccessObject;
+import entity.TaskFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.task.TaskController;
+import interface_adapter.task.TaskPresenter;
+import interface_adapter.task.TaskViewModel;
+import use_case.task.TaskDataAccessInterface;
+import use_case.task.TaskInputBoundary;
+import use_case.task.TaskInteractor;
+import use_case.task.TaskOutputBoundary;
+import view.TaskView;
+
+import java.util.HashMap;
+
+public class TaskUseCaseFactory {
+    private TaskUseCaseFactory() {}
+
+    public static TaskView create(
+            ViewManagerModel viewManagerModel,
+            TaskViewModel taskViewModel,
+            LoggedInViewModel loggedInViewModel
+    ) {
+        TaskController taskController = createTaskUseCase(viewManagerModel, taskViewModel, loggedInViewModel);
+        return new TaskView(taskViewModel, taskController);
+    }
+
+    private static TaskController createTaskUseCase(
+            ViewManagerModel viewManagerModel,
+            TaskViewModel taskViewModel,
+            LoggedInViewModel loggedInViewModel
+    ) {
+        TaskOutputBoundary taskOutputBoundary = new TaskPresenter(
+                taskViewModel, loggedInViewModel, viewManagerModel);
+        TaskFactory taskFactory = new TaskFactory();
+        TaskDataAccessInterface taskDataAccessObject = new FileTaskUserDataAccessObject(new HashMap<>(), taskFactory);
+
+        TaskInputBoundary taskInteractor = new TaskInteractor();
+        return new TaskController(taskInteractor, (TaskPresenter) taskOutputBoundary);
+    }
 }
